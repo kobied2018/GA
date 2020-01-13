f162d28830122453d9f53ee27fbf45e377363e87
k.e <kobied@rnd-hub.com>
2020-01-13 08:15:20 +0200

    new particles can be added to the canvas and move randomly

--- src/MyPSO.jl
+++ src/MyPSO.jl
@@ -2,7 +2,7 @@ using Plots
 gr()
 
 
-struct Particles
+mutable struct Particles
     x::Real
     y::Real
     function Particles(x::Real,y::Real)
@@ -19,44 +19,6 @@ this function create a new particle
 function particles(x::Real, y::Real)::Particles
     return Particles(x,y)
 end # function Particle
-
-
-"""
-    init(width::Real = 600, height::Real = 600)
-
-this function create a plot canvas and place the particle and the
-center of the canvas
-"""
-function init(width::Real = 600, height::Real = 600)
-    particle = particles(width/2,height/2)
-    canvas = plot(size = (width,height))
-end # function init
-
-
-"""
-    update(particle::Particles)
-
-this function update the particle location using the 'move' funciton
-"""
-function update(particle::Particles)
-    move(particle)
-
-    if !draw(particle)
-        init()
-    end
-end # function update
-
-
-"""
-    move(particle::Particles)
-
-this function add a random shift in the particle position
-"""
-function move(particle::Particles)
-    particle.x += 50 * (rand() - 0.5)
-    particle.y += 50 * (rand() - 0.5)
-end # function move
-
 
 struct Bags_size
     w::Real
@@ -76,26 +38,129 @@ end
 rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
 
 """
-    draw(particle::Particles; width::Real = 600, height::Real = 600)
+    init(width::Real, height::Real)
+
+this function create a plot canvas and place the particle and the
+center of the canvas
+"""
+function init(width::Real, height::Real)
+    particle = [particles(0,0)]
+    bag_size= Bags_size(width/3,height/3,-width/6,-height/6)
+    canvas = plot(size = (width,height), legend = false)
+    xlims!(canvas,(-width/2,width/2))
+    ylims!(canvas,(-height/2,height/2))
+    bag = rectangle(bag_size.w,bag_size.h,bag_size.x,bag_size.y)
+    plot!(canvas, bag, opacity = 0.2)
+    draw(particle[1], canvas)
+    return (particle, bag_size, canvas)
+end # function init
+
+
+"""
+    update(particle::Array{Particles})
+
+this function update the particle location using the 'move' funciton
+"""
+function update(particle::Array{Particles}, canvas::Plots.Plot, width::Real, height::Real, bag_size::Bags_size)
+    move(particle)
+    for (ind,p) in enumerate(particle)
+        canvas.series_list[ind + 1][:x] = p.x
+        canvas.series_list[ind + 1][:y] = p.y
+    end
+    if !in_bag(particle, bag_size.sides)
+        init()
+    end
+end # function update
+
+
+"""
+    move(particle::Array{Particles})
+
+this function add a random shift in the particle position
+"""
+function move(particle::Array{Particles})
+    for p in particle
+        p.x += 50 * (rand() - 0.5)
+        p.y += 50 * (rand() - 0.5)
+    end
+    return particle
+end # function move
+
+
+"""
+    draw(particle::Particles, canvas::Plots.Plot)
 
 this function draw the bag and particle
 """
-function draw(particle::Particles; width::Real = 600, height::Real = 600)::Bool
-    bag_size= Bags_size(width/3,height/3,-width/6,-height/6)
-    bag = rectangle(bag_size.w,bag_size.h,bag_size.x,bag_size.y)
-    p = plot(bag, opacity = 0.2, legend = false)
-    xlims!(p,(-width/2,width/2))
-    ylims!(p,(-height/2,height/2))
-    scatter!(p,[particle.x],[particle.y],marker = :rect)
-    return in_bag(particle, bag_sides)
+function draw(particle::Particles, canvas::Plots.Plot)
+    scatter!(canvas,[particle.x],[particle.y],marker = :rect)
 end # function draw
 
 
 """
-    in_bag(particle::Particles, bag_sides::NamedTuple)::Bool
+    in_bag(particle::Array{Particles}, bag_sides::NamedTuple)::Bool
 
 this function chekc if the particle is in the bag
 """
-function in_bag(particle::Particles, bag_sides::NamedTuple)::Bool
-    (bag_sides.left < particle.x < bag_sides.rigth) && (bag_sides.bottom < particle.y < bag_sides.top) && return true
+function in_bag(particle::Array{Particles}, bag_sides::NamedTuple)::Bool
+    res = fill(false,(length(particle),1))
+    for (ind,p) in enumerate(particle)
+        if (bag_sides.left < p.x < bag_sides.rigth) && (bag_sides.bottom < p.y < bag_sides.top)
+            res[ind] = true
+        end
+    end
+    return all(res)
 end # function in_bag
+
+"""
+    addparticle(particle::Array{Particles},canvas::Plots.Plot)
+
+this function add a new particle at the center of the canvas
+"""
+function addparticle(particle::Array{Particles},canvas::Plots.Plot)
+    push!(particle,particles(0,0))
+    draw(particle[end], canvas)
+end # function addparticle
+
+
+"""
+    main(width::Real = 600, height::Real = 600)
+
+documentation
+"""
+function main(width::Real = 600, height::Real = 600)
+    println("To add a new particle press 'n'")
+    println("To add a quit press 'q'")
+    (particle, bag_size, canvas) = init(width, height)
+    @async begin
+        cb(timer) = (update(particle, canvas, width, height, bag_size);display(canvas))
+        global t = Timer(cb,0, interval = 1)
+    end
+    # @async begin
+        while true
+            kb_input = readline()
+            if lowercase(kb_input) == "n"
+                addparticle(particle,canvas)
+                kb_input = ""
+            end
+            if lowercase(kb_input) == "q"
+                close(t)
+                break
+            end
+        end
+    # end
+end # function main
+
+
+"""
+    create_animate()
+
+documentation
+"""
+function create_animate(args)
+    anim = @animate for ii in 1:15
+        Timer(update(particle, canvas, width, height, bag_size),0, interval = 1)
+        display(canvas)
+    end
+    gif(anim, joinpath(@__DIR__,"..","animations\\anim_fps15.gif"), fps = 15)
+end # function
